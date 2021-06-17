db48ea6f84b3e1d6cd99832b6b1b2d5b1a9f417a
andrey <sveshennik@ya.ru>
2021-06-17 19:38:35 +0300

    Запуск проекта из второго урока

---
+++ spring-shop-1/src/main/java/com/example/springshop1/entity/Product.java
@@ -0,0 +1,62 @@
+package com.example.springshop1.entity;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class Product {
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "count")
+    private int count;
+
+    //@Repository
+    //public interface Repo extends CrudRepository<Product, UUID> {
+    //    List<Product> findAll();
+    //}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Product setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Product setCount(int count) {
+        this.count = count;
+        return this;
+    }
+
+    public void incrementCount() {
+        this.count++;
+    }
+
+    public void decreaseCount() {
+        this.count--;
+    }
+}
---
+++ spring-shop-1/src/main/java/com/example/springshop1/frontend/CartView.java
@@ -0,0 +1,51 @@
+package com.example.springshop1.frontend;
+
+import com.example.springshop1.entity.Product;
+import com.example.springshop1.service.CartService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.router.Route;
+
+@Route("cart")
+public class CartView extends VerticalLayout {
+    private final Grid<Product> grid = new Grid<>(Product.class);
+
+    private final CartService cartService;
+
+    public CartView(CartService cartService) {
+        this.cartService = cartService;
+
+        initCartGrid();
+        add(grid);
+    }
+
+    private void initCartGrid() {
+        var products = cartService.getProducts();
+
+        grid.setItems(products);
+        grid.setColumns("name", "count");
+        grid.setSizeUndefined();
+        grid.setSelectionMode(Grid.SelectionMode.MULTI);
+        ListDataProvider<Product> dataProvider = DataProvider.ofCollection(products);
+        grid.setDataProvider(dataProvider);
+
+        grid.addColumn(new ComponentRenderer<>(item -> {
+            var plusButton = new Button("+", i -> {
+                cartService.increaseProductCount(item);
+                grid.getDataProvider().refreshItem(item);
+            });
+
+            var minusButton = new Button("-", i -> {
+                cartService.decreaseProductCount(item);
+                grid.getDataProvider().refreshItem(item);
+            });
+
+            return new HorizontalLayout(plusButton, minusButton);
+        }));
+    }
+}
